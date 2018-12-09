75e80e0bea4c08ceb3f7d082ef2b8516b7895094
radianika <radianika@gmail.com>
2018-12-09 17:54:22 +0200

    Add header section with styles and menu

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//BURGER MENU
+
+////DOM-elements
+let header = document.querySelector('.header');
+let burgerMenu = document.querySelector('.js-menu');
+let openMenuBtn = document.querySelector('.js-burger');
+let closeMenuBtn = burgerMenu.querySelector('.js-close');
+/// classes
+let visibilityClass = 'hidden';
+/// funcrions
+//functions
+function openBurgerMenu(){
+	let headerBox = header.getBoundingClientRect();
+	let headerTop = headerBox.top + pageYOffset;
+	burgerMenu.style.top = headerTop + 'px';
+	toggleMenu();
+}
+
+function toggleMenu(){
+	burgerMenu.classList.toggle(visibilityClass);
+}
+///events
+openMenuBtn.addEventListener('click', openBurgerMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
